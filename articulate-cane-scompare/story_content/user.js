2f674f7935df326551df1f9dabb5afc3b3c803db
davidecorvaglia <davide.corvaglia@gmail.com>
2026-02-04 19:10:59 +0100

    Prove

---
+++ articulate-cane-scompare/story_content/user.js
@@ -0,0 +1,27 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  (function () {
+  if (window.parent?.Virtway?.setVisibility) {
+    window.parent.Virtway.setVisibility("cane", false); // (par. 3.2)  [1](https://intranet3-my.sharepoint.com/personal/davide_corvaglia_windtre_it/Documents/File%20chat%20di%20Microsoft%20Copilot/interaction_with_3d_object_from_external_web%20(002).pdf).pdf).pdf)
+  }
+})();.pdf)
+  }
+})();
+}
+
+};
